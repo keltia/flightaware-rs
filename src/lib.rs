5cd7b8579f7885b5daaee722b5bef80c93759fa6
Ollivier Robert <roberto@keltia.net>
2022-08-30 16:32:11 +0200

    Export versioning stuff.

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,64 @@
 //!
 //! [Flightaware]: https://flightaware.com/
 //!
+//!
+
+use clap::{crate_authors, crate_description, crate_version, StructOpt};
+
+// Our modules
+//
+
+/// API version
+pub const API_VERSION: &str = crate_version!();
+/// Authors
+pub(crate) const AUTHORS: &str = crate_authors!();
+
+/// Display our version banner complete with authors & description
+///
+/// Example:
+/// ```
+/// # use flightaware_rs::description;
+///
+/// println!(description());
+/// ```
+///
+#[inline]
+pub fn description() -> String {
+    format!(
+        "API/{} by {}\n{}",
+        API_VERSION,
+        AUTHORS,
+        crate_description!()
+    )
+}
+
+/// Display our minimal version banner
+///
+/// Example:
+/// ```
+/// # use flightaware_rs::version::version;
+///
+/// println!(version());
+/// ```
+///
+#[inline]
+pub fn version() -> String {
+    format!("API/{}", API_VERSION)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_api_version() {
+        assert_eq!(description(), format!("API/{}", API_VERSION));
+    }
+
+    #[test]
+    fn test_api_description() {
+        assert!(description().contains("API/"));
+        assert!(description().contains(API_VERSION));
+        assert!(description().contains(AUTHORS))
+    }
+}
